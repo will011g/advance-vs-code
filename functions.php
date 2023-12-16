8370f91f0374f1a4949c14206f5ad981df93448e
william <will011g@stud.kea.dk>
2023-12-16 17:20:05 +0100

    advance github

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+// BEGIN ENQUEUE PARENT ACTION
+// AUTO GENERATED - Do not modify or remove comment markers above or below:
+
+if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
+    function chld_thm_cfg_locale_css( $uri ){
+        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
+            $uri = get_template_directory_uri() . '/rtl.css';
+        return $uri;
+    }
+endif;
+add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
+
+// END ENQUEUE PARENT ACTION
+
+function add_cache_control_headers() {
+    header("Cache-Control: max-age=31536000, public");
+}
+add_action('send_headers', 'add_cache_control_headers');
